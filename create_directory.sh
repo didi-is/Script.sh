e283db69192d827f0e56e5e24bd0eb0152f38653
didi-is <ih4683448@gmail.com>
2025-05-07 23:12:27 +0100

    Mise à jour du scripts

--- create_directory.sh
+++ create_directory.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 # Ce script crée un répertoire nommé par l'utilisateur ou "test_directory" par défaut
-read -p "Entrez le nom du répertoire (par défaut : test_directory) : " dir_name
+read -p " ( test_directory) : " dir_name
 dir_name=${dir_name:-test_directory}
-
 if mkdir -p "$dir_name"; then
   echo "Le répertoire '$dir_name' a été créé avec succès."
 else
