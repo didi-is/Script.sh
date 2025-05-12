ec8bd131fc84fab97b01965ab675cc4cbadaca33
didi-is <ih4683448@gmail.com>
2025-05-12 20:52:22 +0100

    mise à jour de scripts

--- create_directory.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Ce script crée un répertoire nommé test_directory
-read -p " test_directory : " dir_name
-dir_name=${dir_name:-test_directory}
-if mkdir -p "$dir_name"; then
-  echo "Le répertoire '$dir_name' a été créé avec succès."
-else
-  echo "Erreur : Impossible de créer le répertoire '$dir_name'."
-fi
--- current_datetime.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Ce script affiche la date et l'heure actuelle au format demandé 
-current_date=$(date +"%Y-%m-%d %H:%M:%S")
-echo "Date et heure actuelles : $current_date"
-
