de356c3ce50520b1ca87033476c2a84e9f912176
didi-is <ih4683448@gmail.com>
2025-05-12 21:33:03 +0100

    mise à jour du scripts

--- list_files_exist.sh
+++ list_files_exist.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-# Script pour lister les fichiers du répertoire courant avec détails
-echo "Liste des fichiers du répertoire courant :"
+#se Script affiche la liste des fichiers du répertoire courant
+
+echo "Liste détaillée des fichiers du répertoire courant :"
 ls -lh --color=auto
+
+echo -e "\nListe uniquement des fichiers (sans les répertoires) :"
+find . -maxdepth 1 -type f
+
+echo -e "\nListe des fichiers triés par date de modification (du plus récent au plus ancien) :"
+ls -lt --color=auto
+
+echo -e "\nListe des fichiers y compris les fichiers cachés :"
+ls -lha --color=auto
