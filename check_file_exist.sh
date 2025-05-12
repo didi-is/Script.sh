a7a01e160ff34e27e8f37f39e8ac589674cfbaa4
didi-is <ih4683448@gmail.com>
2025-05-13 00:18:53 +0100

    mise à jour du scripts

--- check_file_exist.sh
+++ check_file_exist.sh
@@ -4,7 +4,7 @@ echo -n "Entrez le nom du fichier : "
 read fichier
 
 if [ -f "$fichier" ]; then
-    echo "Le fichier'$fichier'existe."
+    echo "Le fichier '$fichier' existe."
 else
-    echo "Le fichier'$fichier'n'existe pas."
+    echo "Le fichier '$fichier' n'existe pas."
 fi
