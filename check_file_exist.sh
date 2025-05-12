0fd2e86303ceb9a3aca4ac60b35a5a0a3fd63870
didi-is <ih4683448@gmail.com>
2025-05-12 23:49:20 +0100

    mise à jour du scripts

--- check_file_exist.sh
+++ check_file_exist.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Ce script verifie si un fichier existe ou non
-echo -n "Entrez le nom du fichier :"
+echo -n "Entrez le nom du fichier : "
 read fichier
 
 if [ -f "$fichier" ]; then
-    echo "Le fichier '$fichier' existe."
+    echo "Le fichier'$fichier'existe."
 else
-    echo "Le fichier '$fichier' n'existe pas."
+    echo "Le fichier'$fichier'n'existe pas."
 fi
